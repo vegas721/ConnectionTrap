04c52eadc6caaf9aa64541a47bc7c3ebf1706b20
vegas721 <130792970+vegas721@users.noreply.github.com>
2025-08-16 16:33:13 +0300

    Update ConnectionTrap.sol
    Fixed curr/prev order (to avoid false positives).

--- ConnectionTrap.sol
+++ ConnectionTrap.sol
@@ -7,31 +7,34 @@ interface ITrap {
 }
 
 contract ConnectionTrap is ITrap {
-    address public constant target = 0x219Bb3aC626B48DCcc62b2bBdD4eCc66A3561E53; //changeble address
+    /// @notice changeblr address
+    address public constant target = 0x219Bb3aC626B48DCcc62b2bBdD4eCc66A3561E53;
 
     struct CollectOutput {
         uint256 balance;
+        uint256 blockNumber;
     }
 
-    /// @notice Собираем текущий баланс кошелька
     function collect() external view override returns (bytes memory) {
         CollectOutput memory o = CollectOutput({
-            balance: target.balance
+            balance: target.balance,
+            blockNumber: block.number
         });
         return abi.encode(o);
     }
 
-    /// @notice Если баланс изменился — сработала новая транзакция
     function shouldRespond(bytes[] calldata data) external pure override returns (bool, bytes memory) {
         if (data.length < 2) {
             return (false, abi.encode("No history"));
         }
 
-        CollectOutput memory prev = abi.decode(data[0], (CollectOutput));
-        CollectOutput memory curr = abi.decode(data[1], (CollectOutput));
+        CollectOutput memory curr = abi.decode(data[0], (CollectOutput)); // latest
+        CollectOutput memory prev = abi.decode(data[1], (CollectOutput)); // older
 
-        if (curr.balance != prev.balance) {
-            return (true, abi.encode("New transaction affecting balance detected"));
+        if (curr.balance > prev.balance) {
+            return (true, abi.encode(target, curr.balance, prev.balance, "Incoming ETH detected"));
+        } else if (curr.balance < prev.balance) {
+            return (true, abi.encode(target, curr.balance, prev.balance, "Outgoing ETH detected"));
         }
 
         return (false, abi.encode(""));
